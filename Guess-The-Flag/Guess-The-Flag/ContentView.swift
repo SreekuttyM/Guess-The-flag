192de485fe5ff52f4dcd4e88bc49ca6bd1ba5f8a
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-15 17:44:58 +0400

    random flag displayed
    ZStack with button and text added

--- Guess-The-Flag/Guess-The-Flag/ContentView.swift
+++ Guess-The-Flag/Guess-The-Flag/ContentView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color.blue
+            VStack {
+                Text("Tap the flag of")
+                    .foregroundStyle(.white)
+
+                Text(countries[correctAnswer])
+                    .foregroundStyle(.white)
+                
+                ForEach(0..<2) { number in
+                        Button {
+                           // flag was tapped
+                        } label: {
+                            Image(countries[number])
+                        }
+                    }
+            }
+            
+            
         }
-        .padding()
     }
+    
 }
 
 #Preview {
