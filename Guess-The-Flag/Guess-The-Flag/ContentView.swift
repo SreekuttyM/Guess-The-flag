723700cfa6bf1e3d7f105c46fa909bfd0d0bdf09
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-16 13:24:13 +0400

    access modifier in contentview

--- Guess-The-Flag/Guess-The-Flag/ContentView.swift
+++ Guess-The-Flag/Guess-The-Flag/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
                     Button {
                         flagTapped(number)
                     } label: {
-                        Image(countries[number]).clipShape(.capsule).shadow(radius: 5)
+                        Image(countries[number]).setFlagImage()
                         
                         
                     }
